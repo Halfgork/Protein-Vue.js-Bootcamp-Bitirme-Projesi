6afdbabd6e6cda1ea31e4529322c85334f926469
Halfgork <gorkemsakalak@gmail.com>
2022-10-29 02:00:43 +0300

    Update labels

---
+++ src/constants/labels.js
@@ -0,0 +1,13 @@
+export const GAME = Object.freeze({
+  GENERATE_PROGRAM: 'Generate Schedule',
+  START: 'Start',
+  PAUSE: 'Pause',
+  RESET: 'Reset',
+  TITLE: 'Horse Racing'
+})
+export const GAMESTATUS = Object.freeze({
+  RUNNING: 'running',
+  NOTSTARTED: 'notStarted',
+  PAUSED: 'paused',
+  FINISHED: 'finished'
+})
