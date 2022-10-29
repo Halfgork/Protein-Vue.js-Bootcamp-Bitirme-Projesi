f89a7d27950831e9116f0325e06b2e8a5fce28db
Halfgork <gorkemsakalak@gmail.com>
2022-10-29 03:58:14 +0300

    Update Game.vue

---
+++ src/components/Game.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="game">
+    <div class="game--top">
+      <GameOptions/>
+    </div>
+    <div class="game--main">
+      <GameBoard/>
+    </div>
+  </div>
+</template>
+
+<script>
+import GameOptions from '@/components/gameOptions/GameOptions'
+import GameBoard from '@/components/gameBoard/GameBoard'
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Game',
+  components: { GameBoard, GameOptions },
+  props: {},
+}
+</script>
+
+<style lang="scss" src="./Game.scss"/>
