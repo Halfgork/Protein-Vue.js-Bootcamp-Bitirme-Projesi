a330380f4390d56e68026689225ba82490642345
Halfgork <gorkemsakalak@gmail.com>
2022-10-29 05:08:33 +0300

    Update Horse.vue and Horse.scss

---
+++ src/components/gameBoard/horse/Horse.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="horse">
+    <h4>Lap {{activeLap && activeLap.lap || '-' }} @ {{activeLap && activeLap.trackLength || '-' }}m </h4>
+    <div v-for="(horse, index) in activeLap && activeLap.horses" :key="index" class="horse-racing">
+      <div class="game-track__content--lane">{{index+1}}</div>
+      <div class="game-track__content--race" :style="{width: `${activeLap.trackLength/4}px`}">
+        <HorseDetails :horse="horse" :width="activeLap.trackLength"/>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import HorseDetails from '@/components/gameBoard/horse/horseDetails/HorseDetails'
+export default {
+  name: 'Horse.vue',
+  components: { HorseDetails },
+  props: {
+    activeLap: {
+      type:Object,
+    }
+  },
+}
+</script>
+
+<style scoped lang="scss" src="./Horse.scss">
+
+</style>
