4fb8721fba0e494406e6ac6e24160ba6d3eaa464
Halfgork <gorkemsakalak@gmail.com>
2022-10-29 04:01:41 +0300

    Update Track.vue and Track.scss

---
+++ src/components/gameBoard/track/Track.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="game-track">
+    <div class="game-track__content">
+      <Horse :activeLap="getActiveLap" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Horse from '@/components/gameBoard/horse/Horse'
+import { mapGetters } from 'vuex'
+export default {
+  name: 'Track',
+  components: { Horse },
+  computed:{
+    ...mapGetters(['getActiveLap'])
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./Track.scss"/>
