393f152bee1a3123f852c9c6c9e161c8ae18994a
Halfgork <gorkemsakalak@gmail.com>
2022-10-29 04:11:00 +0300

    Update ScheduleList.vue and ScheduleList.scss

---
+++ src/components/gameBoard/schedules/ScheduleList.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="schedule-list">
+    <p>Program</p>
+    <ScheduleDetails :schedule="schedule" v-for="(schedule, index) in getScheduleList" :key="index"/>
+  </div>
+</template>
+
+<script>
+import ScheduleDetails from '@/components/gameBoard/schedules/scheduleDetails/ScheduleDetails'
+import { mapGetters } from 'vuex'
+export default {
+  name: 'ScheduleList',
+  components: { ScheduleDetails },
+  computed:{
+    ...mapGetters(['getScheduleList'])
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./ScheduleList.scss"/>
