676e9eddb898102279cd4f2a733553f46409d286
Halfgork <gorkemsakalak@gmail.com>
2022-10-29 04:12:23 +0300

    Update ResultList.vue and ResultList.scss

---
+++ src/components/gameBoard/results/ResultList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="result-list">
+    <p>Results</p>
+    <ResultListDetails :result="result" :runningIndex="index" v-for="(result, index) in getResults" :key="result.lap"/>
+  </div>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+import ResultListDetails from '@/components/gameBoard/results/resultListDetails/ResultListDetails'
+
+export default {
+  name: 'ResultList',
+  components: { ResultListDetails },
+  computed:{
+    ...mapGetters(['getResults'])
+  }
+}
+</script>
+
+<style scoped lang="scss" src="./ResultList.scss"/>
